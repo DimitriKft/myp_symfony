95e9966cefcbd1aaac3d19839bdd2ebcdbd221a7
klopfstein <dimitri.kft@gmail.com>
2020-09-28 15:55:48 +0200

    add data mariadb .env

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ Encore
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
-    // .addStyleEntry('css/style', './assets/scss/app.scss')
+     .addStyleEntry('css/style', './assets/scss/app.scss')
     
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
