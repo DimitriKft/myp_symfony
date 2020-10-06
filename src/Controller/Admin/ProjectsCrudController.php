541822c63fb3babec6579667f5b6698ba12fc3b9
klopfstein <dimitri.kft@gmail.com>
2020-10-06 19:04:12 +0200

    dispay image in dashbord list

--- src/Controller/Admin/ProjectsCrudController.php
+++ src/Controller/Admin/ProjectsCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Projects;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -12,7 +14,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ProjectsCrudController extends AbstractCrudController
 {
@@ -23,18 +24,33 @@ class ProjectsCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        return [
+
+        $imageField = ImageField::new('imageFile')
+        ->setFormType(VichImageType::class)
+        ->setLabel('Image');
+
+        $image = ImageField::new('image')
+            ->setBasePath("/uploads/images")
+            ->setLabel('Image');
+
+
+        $fields = [
             TextField::new('name'),
             TextareaField::new('description'),
-            ImageField::new('imageFile')
-                ->setFormType(VichImageType::class)
-                ->setLabel('Image'),
             TextField::new('repo_url'),
             TextField::new('website_url'),
             DateField::new('createdat'),
             AssociationField::new('user'),
             AssociationField::new('technology')
         ];
+
+        if ($pageName === Crud::PAGE_INDEX || $pageName === Crud::PAGE_DETAIL) {
+            $fields[] = $image;
+        } else {
+            $fields[] = $imageField;
+        }
+
+        return $fields;
     }
     
 }
