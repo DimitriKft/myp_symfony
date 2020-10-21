7bd3959aab231146365ec5018aae0ce705a7282f
klopfstein <dimitri.kft@gmail.com>
2020-10-21 15:45:08 +0200

    update controller usser crud for uplad avatar user

--- src/Controller/Admin/UsersCrudController.php
+++ src/Controller/Admin/UsersCrudController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
-
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UsersCrudController extends AbstractCrudController
 {
@@ -15,17 +17,47 @@ class UsersCrudController extends AbstractCrudController
         return User::class;
     }
 
+    // public function configureFields(string $pageName): iterable
+    // {
+    //     return [
+    //         TextField::new('last_name'),
+    //         TextField::new('first_name'),
+    //         TextField::new('email'),
+    //         TextField::new('password'),
+    //         IntegerField::new('phone'),
+    //         TextField::new('linkedin'),
+    //         TextField::new('github'),
+    //         TextField::new('avatar'),
+    //     ];
+    // }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
+
+        $imageField = ImageField::new('imageFile')
+        ->setFormType(VichImageType::class)
+        ->setLabel('Avatar');
+
+        $image = ImageField::new('avatar')
+            ->setBasePath("/uploads/images")
+            ->setLabel('Avatar');
+
+        $fields = [
             TextField::new('last_name'),
             TextField::new('first_name'),
             TextField::new('email'),
             TextField::new('password'),
-            IntegerField::new('phone'),
+            Textfield::new('phone'),
             TextField::new('linkedin'),
-            TextField::new('github'),
-            TextField::new('avatar'),
+            TextField::new('github')
         ];
+
+        if ($pageName === Crud::PAGE_INDEX || $pageName === Crud::PAGE_DETAIL) {
+            $fields[] = $image;
+        } else {
+            $fields[] = $imageField;
+        }
+
+        return $fields;
     }
 }
